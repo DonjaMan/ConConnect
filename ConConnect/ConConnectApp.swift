df9b50c55c0b8e2f1b07b18a78a9d652019c94a0
Donja <degjay@knology.net>
2026-04-29 15:26:51 -0400

    Initial Commit

---
+++ ConConnect/ConConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConConnectApp.swift
+//  ConConnect
+//
+//  Created by Donald Johnson on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ConConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
